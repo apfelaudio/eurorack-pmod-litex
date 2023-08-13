5fad00d24ec4afcae74a2d17ed095084ba156423
Sebastian Holzapfel <me@vk2seb.com>
2023-08-13 18:29:54 +0200

    Check in Cargo.lock for litex-fw

--- firmware/build.sh
+++ firmware/build.sh
@@ -15,7 +15,7 @@ svd2rust -i $BUILD_DIR/csr.svd --target riscv
 
 # Build the firmware .elf file
 cd $FW_ROOT/litex-fw
-cargo build --target=riscv32imac-unknown-none-elf --release
+cargo build --target=riscv32imac-unknown-none-elf --verbose --release
 
 # Copy it into a binary that litex_term can upload.
 cargo objcopy --release -- -O binary $BUILD_DIR/rust-fw.bin
