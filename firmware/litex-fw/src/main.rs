ef37274ebf55b5192416a57d7e0bccd82708a59b
Sebastian Holzapfel <me@vk2seb.com>
2023-08-13 17:16:30 +0200

    Add basic polysynth example

--- firmware/litex-fw/src/main.rs
+++ firmware/litex-fw/src/main.rs
@@ -3,29 +3,47 @@
 #![allow(dead_code)]
 
 use core::panic::PanicInfo;
-use defmt;
-use litex_hal::prelude::*;
-use litex_hal::uart::UartError;
-use litex_pac as pac;
+
 use riscv;
 use riscv_rt::entry;
 
-const SYSTEM_CLOCK_FREQUENCY: u32 = 12_000_000;
+use litex_hal::prelude::*;
+use litex_pac as pac;
 
-// Globals used by `defmt` logger such that we can log to UART from anywhere.
-static mut ENCODER: defmt::Encoder = defmt::Encoder::new();
-static mut UART_WRITER: Option<Uart> = None;
+use litex_hal::uart::UartError;
+
+use defmt;
+
+use embedded_midi::MidiIn;
+
+use midi_types::*;
+
+use micromath::F32Ext;
+
+use paste::paste;
 
 litex_hal::uart! {
     Uart: litex_pac::UART,
+}
+litex_hal::uart! {
+    UartMidi: litex_pac::UART_MIDI,
 }
 
 litex_hal::timer! {
     Timer: litex_pac::TIMER0,
 }
 
+const SYSTEM_CLOCK_FREQUENCY: u32 = 12_000_000;
+
+static mut ENCODER: defmt::Encoder = defmt::Encoder::new();
+static mut UART_WRITER: Option<UartWriter> = None;
+
 #[defmt::global_logger]
 struct Logger;
+
+struct UartWriter {
+    uart: Uart,
+}
 
 unsafe impl defmt::Logger for Logger {
     fn acquire() {
@@ -55,39 +73,164 @@ fn panic(info: &PanicInfo) -> ! {
 fn do_write(bytes: &[u8]) {
     unsafe {
         if let Some(writer) = &mut UART_WRITER {
-            writer.bwrite_all(bytes).ok();
+            writer.uart.bwrite_all(bytes).ok();
         }
     }
+}
+
+const N_WAVETABLE: u32 = 256;
+const F_A3: f32 = 440f32;
+const F_S: f32 = 93.75f32;
+
+fn volt_to_skip(v: f32) -> u32 {
+    (((N_WAVETABLE as f32 * F_A3) / F_S) * 2.0f32.powf(v - 3.75f32)) as u32
+}
+
+fn note_to_volt(midi_note: Note) -> f32 {
+    let result = (((3 * 12) + u8::from(midi_note)) - 69) as f32 / 12.0f32;
+    if result > 0.0f32 {
+        result
+    } else {
+        0.0f32
+    }
+}
+
+const N_VOICES: usize = 4;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum VoiceState {
+    Off,
+    On(Note, u32),
+}
+
+struct VoiceManager {
+    voices: [VoiceState; N_VOICES],
+}
+
+impl VoiceManager {
+    // For now we don't cull the oldest notes...
+    fn note_on(&mut self, note: Note) {
+        for v in self.voices.iter_mut() {
+            if *v == VoiceState::Off {
+                let skip = volt_to_skip(note_to_volt(note)) as u32;
+                *v = VoiceState::On(note, skip);
+                return;
+            }
+        }
+    }
+
+    fn note_off(&mut self, note: Note) {
+        for v in self.voices.iter_mut() {
+            if let VoiceState::On(v_note, _) = *v {
+                if note == v_note {
+                    *v = VoiceState::Off;
+                }
+            }
+        }
+    }
+}
+
+macro_rules! csr_read_n {
+    ($periph:ident, $module:ident, $field:ident) => {
+        paste! {
+            [
+                $periph.$module.[<$field 0>].read().bits(),
+                $periph.$module.[<$field 1>].read().bits(),
+                $periph.$module.[<$field 2>].read().bits(),
+                $periph.$module.[<$field 3>].read().bits(),
+            ]
+        }
+    };
+}
+
+macro_rules! csr_write_n {
+    ($periph:ident, $module:ident, $index:expr, $field:ident, $value:expr) => {
+        paste! {
+            unsafe {
+                match $index {
+                    0 => $periph.[<$module 0>].$field.write(|w| w.$field().bits($value)),
+                    1 => $periph.[<$module 1>].$field.write(|w| w.$field().bits($value)),
+                    2 => $periph.[<$module 2>].$field.write(|w| w.$field().bits($value)),
+                    3 => $periph.[<$module 3>].$field.write(|w| w.$field().bits($value)),
+                    _ => ()
+                }
+            }
+        }
+    };
 }
 
 #[entry]
 fn main() -> ! {
     let peripherals = unsafe { pac::Peripherals::steal() };
 
+    let uart_midi = UartMidi::new(peripherals.UART_MIDI);
+
     unsafe {
-        UART_WRITER = Some(Uart::new(peripherals.UART));
+        UART_WRITER = Some(UartWriter {
+            uart: Uart::new(peripherals.UART),
+        });
         if let Some(writer) = &mut UART_WRITER {
             writer
+                .uart
                 .bwrite_all(b"hello from litex-fw! dropping to defmt logging --\n")
                 .ok();
         }
     }
 
-    let mut elapsed: f32 = 0.0f32;
+    let mut midi_in = MidiIn::new(uart_midi);
 
-    let mut timer = Timer::new(peripherals.TIMER0, SYSTEM_CLOCK_FREQUENCY);
+    let mut voice_manager = VoiceManager {
+        voices: [VoiceState::Off; N_VOICES],
+    };
 
-    defmt::info!("Starting main loop --");
-
+    defmt::info!("Peripherals initialized");
     loop {
-        defmt::info!("jack_detect {=u8:x}", peripherals.EURORACK_PMOD0.csr_jack.read().bits() as u8);
-        defmt::info!("input0 {}", peripherals.EURORACK_PMOD0.csr_cal_in0.read().bits() as i16);
-        defmt::info!("input1 {}", peripherals.EURORACK_PMOD0.csr_cal_in1.read().bits() as i16);
-        defmt::info!("input2 {}", peripherals.EURORACK_PMOD0.csr_cal_in2.read().bits() as i16);
-        defmt::info!("input3 {}", peripherals.EURORACK_PMOD0.csr_cal_in3.read().bits() as i16);
-        defmt::info!("serial {=u32:x}", peripherals.EURORACK_PMOD0.csr_eeprom_serial.read().bits() as u32);
-        timer.delay_ms(1000u32);
-        defmt::info!("tick - elapsed {} sec", elapsed);
-        elapsed += 1.0f32;
+        let event = midi_in.read();
+
+        //defmt::info!("MIDI event: {:?}", defmt::Debug2Format(&event));
+        //update_skip = Some(volt_to_skip(note_to_volt(note)) as u32)
+        match event {
+            Ok(MidiMessage::NoteOn(_, note, velocity)) => {
+                defmt::info!(
+                    "note on: note={} vel={}",
+                    u8::from(note),
+                    u8::from(velocity)
+                );
+                voice_manager.note_on(note);
+            }
+            Ok(MidiMessage::NoteOff(_, note, velocity)) => {
+                defmt::info!(
+                    "note off: note={} vel={}",
+                    u8::from(note),
+                    u8::from(velocity)
+                );
+                voice_manager.note_off(note);
+            }
+            _ => {}
+        }
+
+        let ins = csr_read_n!(peripherals, EURORACK_PMOD0, csr_cal_in).map(|x| x as i16);
+
+        for n_voice in 0..=3 {
+            let mut write_skip = 0u32;
+            if let VoiceState::On(_, skip) = voice_manager.voices[n_voice] {
+                write_skip = skip;
+            }
+            csr_write_n!(
+                peripherals,
+                WAVETABLE_OSCILLATOR,
+                n_voice,
+                csr_wavetable_inc,
+                write_skip
+            );
+            csr_write_n!(peripherals, KARLSEN_LPF, n_voice, csr_g, ins[1] as u16);
+            csr_write_n!(
+                peripherals,
+                KARLSEN_LPF,
+                n_voice,
+                csr_resonance,
+                ins[2] as u16
+            );
+        }
     }
 }
