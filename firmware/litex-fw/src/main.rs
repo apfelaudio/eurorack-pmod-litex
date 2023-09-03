c5ad6d1d829fc4e57a970efc9c21dd0733fa213c
Sebastian Holzapfel <me@vk2seb.com>
2023-09-03 13:33:46 +0200

    voice and filter visualization

--- firmware/litex-fw/src/main.rs
+++ firmware/litex-fw/src/main.rs
@@ -15,7 +15,7 @@ use ufmt;
 
 use embedded_graphics::{
     pixelcolor::{Gray4, GrayColor},
-    primitives::{PrimitiveStyle, PrimitiveStyleBuilder, Rectangle},
+    primitives::{PrimitiveStyle, PrimitiveStyleBuilder, Rectangle, Triangle, Line},
     mono_font::{ascii::FONT_4X6, ascii::FONT_5X7, MonoTextStyle},
     prelude::*,
     text::{Alignment, Text},
@@ -59,6 +59,111 @@ litex_hal::gpio! {
 
 litex_hal::spi! {
     SPI: (litex_pac::OLED_SPI, u8),
+}
+
+fn draw_voice<D>(d: &mut D, sy: u32, ix: u32, voice: &Voice) -> Result<(), D::Error>
+where
+    D: DrawTarget<Color = Gray4>,
+{
+
+    let thin_stroke = PrimitiveStyle::with_stroke(Gray4::WHITE, 1);
+    let thin_stroke_grey = PrimitiveStyleBuilder::new()
+        .stroke_color(Gray4::new(0x3))
+        .stroke_width(1)
+        .build();
+    let character_style = MonoTextStyle::new(&FONT_4X6, Gray4::WHITE);
+    let character_style_h = MonoTextStyle::new(&FONT_5X7, Gray4::WHITE);
+    let title_y = 10u32;
+    let box_h = 20u32;
+    let box_y = title_y + 3u32 + box_h;
+
+    let mut s: String<32> = String::new();
+
+    Rectangle::new(Point::new(2, sy as i32), Size::new(60, box_y))
+        .into_styled(thin_stroke_grey)
+        .draw(d)?;
+
+    Rectangle::new(Point::new(2, sy as i32), Size::new(60, title_y))
+        .into_styled(thin_stroke)
+        .draw(d)?;
+
+    ufmt::uwrite!(&mut s, "CH {}", ix).ok();
+
+    Text::with_alignment(
+        &s,
+        Point::new(d.bounding_box().center().x, (sy as i32)+7),
+        character_style_h,
+        Alignment::Center,
+    )
+    .draw(d)?;
+
+
+    let mut stroke_gain = PrimitiveStyleBuilder::new()
+        .stroke_color(Gray4::new(0x1))
+        .stroke_width(1)
+        .build();
+
+    let mut stroke_idle = PrimitiveStyleBuilder::new()
+        .stroke_color(Gray4::new(0x1))
+        .stroke_width(1)
+        .build();
+
+    s.clear();
+    if voice.state != VoiceState::Idle {
+        let semitones = voice.note as i32 - 60i32;
+        if semitones > 0 {
+            ufmt::uwrite!(&mut s, "+").ok();
+        }
+        ufmt::uwrite!(&mut s, "{}", semitones).ok();
+
+        stroke_gain = PrimitiveStyleBuilder::new()
+            .stroke_color(Gray4::new((15f32 * voice.amplitude) as u8))
+            .stroke_width(1)
+            .build();
+
+        stroke_idle = PrimitiveStyleBuilder::new()
+            .stroke_color(Gray4::WHITE)
+            .stroke_width(1)
+            .build();
+    }
+
+    let filter_pos: i32 = (20f32 * voice.amplitude) as i32;
+
+    Line::new(Point::new(38, sy as i32 + 16),
+              Point::new(40+filter_pos-2, sy as i32 + 16))
+              .into_styled(stroke_gain)
+              .draw(d)?;
+
+    Line::new(Point::new(40+filter_pos, sy as i32 + 24),
+              Point::new(55, sy as i32 + 24))
+              .into_styled(stroke_gain)
+              .draw(d)?;
+
+    Line::new(Point::new(40+filter_pos-2, sy as i32 + 16),
+              Point::new(40+filter_pos, sy as i32 + 24))
+              .into_styled(stroke_gain)
+              .draw(d)?;
+
+    Rectangle::new(Point::new(7, sy as i32 + 15), Size::new(19, 11))
+        .into_styled(stroke_idle)
+        .draw(d)?;
+
+    Text::new(
+        &s,
+        Point::new(9, sy as i32 + 22),
+        character_style_h,
+    )
+    .draw(d)?;
+
+    /*
+    Triangle::new(Point::new(35, sy as i32 + 15),
+                  Point::new(35, sy as i32 + 15 + 10),
+                  Point::new(50, sy as i32 + 20))
+        .into_styled(stroke_gain)
+        .draw(d)?;
+        */
+
+    Ok(())
 }
 
 fn draw_titlebox<D>(d: &mut D, sy: u32, title: &str, fields: &[&str], values: &[u32]) -> Result<(), D::Error>
@@ -212,6 +317,14 @@ fn main() -> ! {
 
     loop {
 
+        Text::with_alignment(
+            "POLYPHONIZER\nDEMO\n-apfelaudio-",
+            Point::new(disp.bounding_box().center().x, 10),
+            character_style,
+            Alignment::Center,
+        )
+        .draw(&mut disp).ok();
+
         v += 3;
 
         for i in 0..=15 {
@@ -238,15 +351,9 @@ fn main() -> ! {
             shifter[n_voice].set_pitch(voice.pitch);
             lpf[n_voice].set_cutoff((voice.amplitude * 8000f32) as i16);
             lpf[n_voice].set_resonance(10000i16);
+            draw_voice(&mut disp, (35+35*n_voice) as u32, n_voice as u32, voice).ok();
         }
 
-        Text::with_alignment(
-            "<TEST UTIL>",
-            Point::new(disp.bounding_box().center().x, 10),
-            character_style,
-            Alignment::Center,
-        )
-        .draw(&mut disp).ok();
 
         if peripherals.ENCODER_BUTTON.in_.read().bits() != 0 {
             peripherals.CTRL.reset.write(|w| w.soc_rst().bit(true));
@@ -293,7 +400,6 @@ fn main() -> ! {
             pmod0.input(2) as u32,
             pmod0.input(3) as u32,
         ]).ok();
-        */
 
         draw_titlebox(&mut disp, 190, "MIDI", &[
           "v0",
@@ -306,6 +412,7 @@ fn main() -> ! {
           v[2].into(),
           v[3].into()
         ]).ok();
+        */
 
         if let Some(value) = td_us {
             let mut s: String<32> = String::new();
