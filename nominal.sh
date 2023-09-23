f8c555254a577b1b27a08585ecb2a87f8f84bd50
Sebastian Holzapfel <me@sebholzapfel.com>
2023-09-23 13:24:49 +0200

    sim/ecpix switch from minimal->lite vex, works

--- nominal.sh
+++ nominal.sh
@@ -3,7 +3,7 @@ set -e
 
 # SVD only
 echo "Generate SVD"
-./sim.py --with-sdram --cpu-type vexriscv --cpu-variant minimal --csr-svd build/sim/csr.svd --no-compile-gateware
+./sim.py --with-sdram --cpu-type vexriscv --cpu-variant lite --csr-svd build/sim/csr.svd --no-compile-gateware
 # FW only
 echo "Build FW"
 cd firmware
@@ -11,4 +11,4 @@ OBJCOPY=riscv64-linux-gnu-objcopy BOARD=sim ./build.sh
 cd ..
 # Boot firmware after BIOS
 echo "Simulate GW+FW"
-./sim.py --with-sdram --sdram-init build/sim/rust-fw.bin --cpu-type vexriscv --cpu-variant minimal
+./sim.py --with-sdram --sdram-init build/sim/rust-fw.bin --cpu-type vexriscv --cpu-variant lite
