b2eeff814b08fa7ef493551584d07a3bd312451b
Sebastian Holzapfel <me@sebholzapfel.com>
2023-09-24 11:18:10 +0200

    Fix channel mapping

--- firmware/litex-fw/src/main.rs
+++ firmware/litex-fw/src/main.rs
@@ -105,7 +105,6 @@ fn main() -> ! {
             }
         }
         */
-        /*
         log::info!("READ");
         unsafe {
             asm!("fence iorw, iorw");
@@ -114,8 +113,7 @@ fn main() -> ! {
 
             }
         }
-        timer.delay_ms(10u32);
-        */
+        timer.delay_ms(500u32);
         /*
         log::info!("jack_detect {:x}", peripherals.EURORACK_PMOD0.csr_jack.read().bits() as u8);
         log::info!("input0 {}", peripherals.EURORACK_PMOD0.csr_cal_in0.read().bits() as i16);
