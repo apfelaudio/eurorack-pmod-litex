6b3198c9a0b1bda92d77c7b9cbe06c6fc9b4835a
Sebastian Holzapfel <me@sebholzapfel.com>
2023-09-27 10:12:19 +0200

    irq_load: track outside handler

--- firmware/litex-fw/src/main.rs
+++ firmware/litex-fw/src/main.rs
@@ -37,12 +37,24 @@ static mut BUF_OUT: Aligned<A4, [i16; BUF_SZ_SAMPLES]> = Aligned([0i16; BUF_SZ_S
 
 static mut VULTA: Option<libvult::VultDsp> = None;
 static mut VULTB: Option<libvult::VultDsp> = None;
+static mut VULTC: Option<libvult::VultDsp> = None;
+static mut VULTD: Option<libvult::VultDsp> = None;
+
+static mut LAST_IRQ: u32 = 0;
+static mut LAST_IRQ_LEN: u32 = 0;
+static mut LAST_IRQ_PERIOD: u32 = 0;
 
 #[export_name = "DefaultHandler"]
 unsafe fn irq_handler() {
 
+
     let pending_irq = vexriscv::register::vmip::read();
     let peripherals = pac::Peripherals::steal();
+
+    peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
+    let trace = peripherals.TIMER0.uptime_cycles0.read().bits();
+    LAST_IRQ_PERIOD = trace - LAST_IRQ;
+    LAST_IRQ = trace;
 
     if (pending_irq & (1 << pac::Interrupt::DMA_ROUTER0 as usize)) != 0 {
         let offset = peripherals.DMA_ROUTER0.offset_words.read().bits();
@@ -50,35 +62,35 @@ unsafe fn irq_handler() {
 
         if let Some(ref mut vulta) = VULTA {
         if let Some(ref mut vultb) = VULTB {
+        if let Some(ref mut vultc) = VULTC {
+        if let Some(ref mut vultd) = VULTD {
 
             if offset as usize == ((BUF_SZ_WORDS/2)+1) {
                 for i in 0..(BUF_SZ_SAMPLES/2) {
                     BUF_OUT[i] = match i % 4 {
                         0 => vulta.process(BUF_IN[i]),
                         1 => vultb.process(BUF_IN[i]),
+                        2 => vultc.process(BUF_IN[i]),
+                        3 => vultd.process(BUF_IN[i]),
                         _ => 0
                     }
                 }
             }
-
-            peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
-            let trace_start = peripherals.TIMER0.uptime_cycles0.read().bits();
 
             if offset as usize == (BUF_SZ_WORDS-1) {
                 for i in (BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES) {
                     BUF_OUT[i] = match i % 4 {
                         0 => vulta.process(BUF_IN[i]),
                         1 => vultb.process(BUF_IN[i]),
+                        2 => vultc.process(BUF_IN[i]),
+                        3 => vultd.process(BUF_IN[i]),
                         _ => 0
                     }
                 }
             }
 
-            peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
-            let trace_end = peripherals.TIMER0.uptime_cycles0.read().bits();
-            let trace_diff = trace_end - trace_start;
-            // cycles for half the samples
-            info!("{} {}", trace_diff, trace_diff >> 8);
+        }
+        }
         }
         }
 
@@ -87,6 +99,10 @@ unsafe fn irq_handler() {
         asm!("fence iorw, iorw");
         asm!(".word(0x500F)");
     }
+
+    peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
+    let trace_end = peripherals.TIMER0.uptime_cycles0.read().bits();
+    LAST_IRQ_LEN = trace_end - trace;
 }
 
 
@@ -108,6 +124,8 @@ fn main() -> ! {
     unsafe {
         VULTA = Some(libvult::VultDsp::new());
         VULTB = Some(libvult::VultDsp::new());
+        VULTC = Some(libvult::VultDsp::new());
+        VULTD = Some(libvult::VultDsp::new());
 
         peripherals.DMA_ROUTER0.base_writer.write(|w| w.bits(BUF_IN.as_mut_ptr() as u32));
         peripherals.DMA_ROUTER0.base_reader.write(|w| w.bits(BUF_OUT.as_ptr() as u32));
@@ -126,16 +144,15 @@ fn main() -> ! {
     }
 
     loop {
-        /*
-        log::info!("READ");
         unsafe {
             asm!("fence iorw, iorw");
             for i in 0..4 {
                 log::info!("{:x}@{:x}", i, BUF_IN[i]);
-
             }
+            log::info!("irq_period: {}", LAST_IRQ_PERIOD);
+            log::info!("irq_len: {}", LAST_IRQ_LEN);
+            log::info!("irq_load_percent: {}", (LAST_IRQ_LEN * 100) / LAST_IRQ_PERIOD);
         }
         timer.delay_ms(500u32);
-        */
     }
 }
