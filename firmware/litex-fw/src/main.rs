86be1f439c29ece45f02bf79186791ac8fc9360c
Sebastian Holzapfel <me@vk2seb.com>
2023-10-04 09:28:00 +0200

    switch to bleeding edge PLIC configuration from riscv crate, still works

--- firmware/litex-fw/src/main.rs
+++ firmware/litex-fw/src/main.rs
@@ -10,8 +10,6 @@ use riscv_rt::entry;
 use riscv;
 use core::arch::asm;
 use aligned_array::{Aligned, A4};
-use vexriscv;
-use plic::Plic;
 
 mod log;
 mod libvult;
@@ -41,24 +39,65 @@ static mut VULTB: Option<libvult::VultDsp> = None;
 static mut VULTC: Option<libvult::VultDsp> = None;
 static mut VULTD: Option<libvult::VultDsp> = None;
 
-static mut PLIC: Option<&'static Plic> = None;
-
 static mut LAST_IRQ: u32 = 0;
 static mut LAST_IRQ_LEN: u32 = 0;
 static mut LAST_IRQ_PERIOD: u32 = 0;
 
+#[derive(Copy, Clone)]
+pub struct VexInterrupt {
+    pac_irq: pac::Interrupt,
+}
+
+unsafe impl riscv::peripheral::plic::InterruptNumber for VexInterrupt {
+    const MAX_INTERRUPT_NUMBER: u16 = 3u16;
+
+    fn number(self) -> u16 {
+        self.pac_irq as u16
+    }
+
+    fn try_from(value: u16) -> Result<Self, u16> {
+        if let Ok(value_u8) = value.try_into() {
+            match pac::Interrupt::try_from(value_u8) {
+                Ok(result) => Ok(VexInterrupt { pac_irq: result } ),
+                Err(_) => Err(value)
+            }
+        } else {
+            Err(value)
+        }
+    }
+}
+
+impl From<pac::Interrupt> for VexInterrupt {
+    fn from(pac_irq: pac::Interrupt) -> Self {
+        VexInterrupt { pac_irq }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct VexPriority {
+    prio: u8,
+}
+
+unsafe impl riscv::peripheral::plic::PriorityNumber for VexPriority {
+    const MAX_PRIORITY_NUMBER: u8 = 0xFFu8;
+    fn number(self) -> u8 { self.prio }
+    fn try_from(value: u8) -> Result<Self, u8> { Ok(VexPriority{prio: value}) }
+}
+
+impl From<u8> for VexPriority {
+    fn from(prio: u8) -> Self {
+        VexPriority { prio }
+    }
+}
+
+riscv::plic_context!(PLIC0, 0xf0c0_0000, 0, VexInterrupt, VexPriority);
+static mut PLIC: Option<PLIC0> = None;
+
 #[export_name = "DefaultHandler"]
 unsafe fn irq_handler() {
 
-
-    //let pending_irq = vexriscv::register::vmip::read();
-    let plic: &'static Plic = PLIC.unwrap();
-    // Claim for core 0
-    let pending_irq_opt = plic.claim(0);
-    let mut pending_irq = 0;
-    if let Some(irq) = pending_irq_opt {
-        pending_irq = irq.into();
-    }
+    // This should only fail if we have multiple cores claiming things
+    let pending_irq = PLIC0::claim().unwrap();
     let peripherals = pac::Peripherals::steal();
 
     peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
@@ -66,8 +105,8 @@ unsafe fn irq_handler() {
     LAST_IRQ_PERIOD = trace - LAST_IRQ;
     LAST_IRQ = trace;
 
-    match pending_irq {
-        3 => {
+    match pending_irq.pac_irq {
+        pac::Interrupt::DMA_ROUTER0 => {
             let offset = peripherals.DMA_ROUTER0.offset_words.read().bits();
             let pending_subtype = peripherals.DMA_ROUTER0.ev_pending.read().bits();
 
@@ -79,12 +118,12 @@ unsafe fn irq_handler() {
                 if offset as usize == ((BUF_SZ_WORDS/2)+1) {
                     for i in 0..(BUF_SZ_SAMPLES/2) {
                         BUF_OUT[i] = match i % 4 {
-                            /*
                             0 => vulta.process(BUF_IN[i]),
-                            */
+                            /*
                             1 => vultb.process(BUF_IN[i]),
                             2 => vultc.process(BUF_IN[i]),
                             3 => vultd.process(BUF_IN[i]),
+                            */
                             _ => 0
                         }
                     }
@@ -93,12 +132,12 @@ unsafe fn irq_handler() {
                 if offset as usize == (BUF_SZ_WORDS-1) {
                     for i in (BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES) {
                         BUF_OUT[i] = match i % 4 {
-                            /*
                             0 => vulta.process(BUF_IN[i]),
-                            */
+                            /*
                             1 => vultb.process(BUF_IN[i]),
                             2 => vultc.process(BUF_IN[i]),
                             3 => vultd.process(BUF_IN[i]),
+                            */
                             _ => 0
                         }
                     }
@@ -123,13 +162,9 @@ unsafe fn irq_handler() {
     let trace_end = peripherals.TIMER0.uptime_cycles0.read().bits();
     LAST_IRQ_LEN = trace_end - trace;
 
-    // Core 0, did 1 IRQ
-    plic.complete(0, pending_irq);
+    PLIC0::complete(pending_irq);
 }
 
-pub const unsafe fn plic_from_addr(addr: usize) -> &'static Plic {
-    &*(addr as *const Plic)
-}
 
 #[entry]
 fn main() -> ! {
@@ -148,15 +183,6 @@ fn main() -> ! {
 
     unsafe {
 
-        let plic = plic_from_addr(0xf0c0_0000);
-        // First 0 is hart context CORES_0_EXTERNAL_INTERRUPT
-        plic.set_threshold(0, 0);
-        // Is the indexing correct?
-        plic.set_priority(pac::Interrupt::DMA_ROUTER0 as u32,  1);
-        // IRQ source, Hart context
-        plic.enable(pac::Interrupt::DMA_ROUTER0 as u32, 0);
-
-        PLIC = Some(plic);
 
         VULTA = Some(libvult::VultDsp::new());
         VULTB = Some(libvult::VultDsp::new());
@@ -169,12 +195,17 @@ fn main() -> ! {
         peripherals.DMA_ROUTER0.enable.write(|w| w.bits(1u32));
         peripherals.DMA_ROUTER0.ev_enable.write(|w| w.half().bit(true));
 
-        // Enable interrupts from DMA router (vexriscv specific register)
-        //vexriscv::register::vmim::write(1 << (pac::Interrupt::DMA_ROUTER0 as usize));
+
+        // PLIC configuration
+        let mut plic = PLIC0::new();
+        let dma_irq = VexInterrupt::from(pac::Interrupt::DMA_ROUTER0);
+        plic.set_threshold(VexPriority::from(0));
+        plic.set_priority(dma_irq, VexPriority::from(1));
+        plic.enable_interrupt(dma_irq);
 
         // Enable machine external interrupts (basically everything added on by LiteX).
+        // Including the PLIC.
         riscv::register::mie::set_mext();
-
         // Finally enable interrupts.
         riscv::interrupt::enable();
     }
