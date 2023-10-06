6f71d50c899bbc26b7fe46d2ec4aacd7b07b5c12
Sebastian Holzapfel <me@sebholzapfel.com>
2023-10-06 20:08:24 +0200

    fixed point karlsen works

--- firmware/litex-fw/src/main.rs
+++ firmware/litex-fw/src/main.rs
@@ -12,6 +12,8 @@ use core::arch::asm;
 use aligned_array::{Aligned, A4};
 use vexriscv;
 
+use fixed::{FixedI32, types::extra::U16};
+
 mod log;
 use log::*;
 
@@ -28,6 +30,7 @@ litex_hal::timer! {
 const N_CHANNELS: usize = 4;
 const BUF_SZ_WORDS: usize = 512;
 const BUF_SZ_SAMPLES: usize = BUF_SZ_WORDS * 2;
+const BUF_SZ_SAMPLES_PER_IRQ: usize = BUF_SZ_SAMPLES / 2;
 
 // MUST be aligned to 4-byte (word) boundary for RV32. These buffers are directly
 // accessed by DMA that iterates across words!.
@@ -37,6 +40,61 @@ static mut BUF_OUT: Aligned<A4, [i16; BUF_SZ_SAMPLES]> = Aligned([0i16; BUF_SZ_S
 static mut LAST_IRQ: u32 = 0;
 static mut LAST_IRQ_LEN: u32 = 0;
 static mut LAST_IRQ_PERIOD: u32 = 0;
+
+static mut KARLSEN: Option<KarlsenLpf> = None;
+
+type Fix = FixedI32<U16>;
+
+struct KarlsenLpf {
+    rezz: Fix,
+    sat : Fix,
+    a1  : Fix,
+    a2  : Fix,
+    a3  : Fix,
+    a4  : Fix,
+}
+
+impl KarlsenLpf {
+    fn new() -> Self {
+        KarlsenLpf {
+            rezz: Fix::from_num(0),
+            sat: Fix::from_num(0),
+            a1: Fix::from_num(0),
+            a2: Fix::from_num(0),
+            a3: Fix::from_num(0),
+            a4: Fix::from_num(0)
+        }
+    }
+
+    fn proc(&mut self, x: Fix, g: Fix, res: Fix) -> Fix {
+        let gmax = Fix::max(Fix::from_num(0), g);
+        let res_scaled = res * 4;
+        self.rezz = x - ((self.a4 - x) * res_scaled);
+        self.sat = Fix::min(Fix::ONE, self.rezz);
+        self.sat = Fix::max(-Fix::ONE, self.rezz);
+        self.a1 = self.a1 + ((-self.a1 + self.sat) * gmax);
+        self.a2 = self.a2 + ((-self.a2 + self.a1) * gmax);
+        self.a3 = self.a3 + ((-self.a3 + self.a2) * gmax);
+        self.a4 = self.a4 + ((-self.a4 + self.a3) * gmax);
+        self.a4
+    }
+}
+
+fn process(dsp: &mut KarlsenLpf, buf_out: &mut [i16], buf_in: &[i16]) {
+    for i in 0..(buf_in.len()/N_CHANNELS) {
+        let x_in: [Fix; N_CHANNELS] = [
+            Fix::from_bits(buf_in[N_CHANNELS*i+0] as i32),
+            Fix::from_bits(buf_in[N_CHANNELS*i+1] as i32),
+            Fix::from_bits(buf_in[N_CHANNELS*i+2] as i32),
+            Fix::from_bits(buf_in[N_CHANNELS*i+3] as i32),
+        ];
+        let y: Fix = dsp.proc(x_in[0], x_in[1], x_in[2]);
+        buf_out[N_CHANNELS*i+0] = y.to_bits() as i16;
+        buf_out[N_CHANNELS*i+1] = x_in[1].to_bits() as i16;
+        buf_out[N_CHANNELS*i+2] = x_in[2].to_bits() as i16;
+        buf_out[N_CHANNELS*i+3] = x_in[3].to_bits() as i16;
+    }
+}
 
 #[export_name = "DefaultHandler"]
 unsafe fn irq_handler() {
@@ -53,15 +111,17 @@ unsafe fn irq_handler() {
         let offset = peripherals.DMA_ROUTER0.offset_words.read().bits();
         let pending_subtype = peripherals.DMA_ROUTER0.ev_pending.read().bits();
 
-        if offset as usize == ((BUF_SZ_WORDS/2)+1) {
-            for i in 0..(BUF_SZ_SAMPLES/2) {
-                BUF_OUT[i] = BUF_IN[i];
+        if let Some(ref mut dsp) = KARLSEN {
+            if offset as usize == ((BUF_SZ_WORDS/2)+1) {
+                process(dsp,
+                        &mut BUF_OUT[0..(BUF_SZ_SAMPLES/2)],
+                        &BUF_IN[0..(BUF_SZ_SAMPLES/2)]);
             }
-        }
 
-        if offset as usize == (BUF_SZ_WORDS-1) {
-            for i in (BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES) {
-                BUF_OUT[i] = BUF_IN[i];
+            if offset as usize == (BUF_SZ_WORDS-1) {
+                process(dsp,
+                        &mut BUF_OUT[(BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES)],
+                        &BUF_IN[(BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES)]);
             }
         }
 
@@ -93,6 +153,8 @@ fn main() -> ! {
     }
 
     unsafe {
+        KARLSEN = Some(KarlsenLpf::new());
+
         peripherals.DMA_ROUTER0.base_writer.write(|w| w.bits(BUF_IN.as_mut_ptr() as u32));
         peripherals.DMA_ROUTER0.base_reader.write(|w| w.bits(BUF_OUT.as_ptr() as u32));
         peripherals.DMA_ROUTER0.length_words.write(|w| w.bits(BUF_SZ_WORDS as u32));
