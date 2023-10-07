1bf79d42f8c854ccaa291af214fb50a10f06e25d
Sebastian Holzapfel <me@sebholzapfel.com>
2023-10-07 10:19:33 +0200

    nax runs without DMA working

--- firmware/litex-fw/src/main.rs
+++ firmware/litex-fw/src/main.rs
@@ -10,7 +10,6 @@ use riscv_rt::entry;
 use riscv;
 use core::arch::asm;
 use aligned_array::{Aligned, A4};
-use vexriscv;
 
 use fixed::{FixedI32, types::extra::U16};
 
@@ -40,6 +39,59 @@ static mut BUF_OUT: Aligned<A4, [i16; BUF_SZ_SAMPLES]> = Aligned([0i16; BUF_SZ_S
 static mut LAST_IRQ: u32 = 0;
 static mut LAST_IRQ_LEN: u32 = 0;
 static mut LAST_IRQ_PERIOD: u32 = 0;
+
+#[derive(Copy, Clone)]
+pub struct VexInterrupt {
+    pac_irq: pac::Interrupt,
+}
+
+unsafe impl riscv::peripheral::plic::InterruptNumber for VexInterrupt {
+    // TODO: this is PLIC max -- fetch this from PAC?
+    const MAX_INTERRUPT_NUMBER: u16 = 32u16;
+
+    fn number(self) -> u16 {
+        self.pac_irq as u16
+    }
+
+    fn try_from(value: u16) -> Result<Self, u16> {
+        if let Ok(value_u8) = value.try_into() {
+            match pac::Interrupt::try_from(value_u8) {
+                Ok(result) => Ok(VexInterrupt { pac_irq: result } ),
+                Err(_) => Err(value)
+            }
+        } else {
+            Err(value)
+        }
+    }
+}
+
+impl From<pac::Interrupt> for VexInterrupt {
+    fn from(pac_irq: pac::Interrupt) -> Self {
+        VexInterrupt { pac_irq }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct VexPriority {
+    prio: u8,
+}
+
+unsafe impl riscv::peripheral::plic::PriorityNumber for VexPriority {
+    // TODO: Customize Vex for more priority numbers?
+    const MAX_PRIORITY_NUMBER: u8 = 0x3u8;
+    fn number(self) -> u8 { self.prio }
+    fn try_from(value: u8) -> Result<Self, u8> { Ok(VexPriority{prio: value}) }
+}
+
+impl From<u8> for VexPriority {
+    fn from(prio: u8) -> Self {
+        VexPriority { prio }
+    }
+}
+
+riscv::plic_context!(PLIC0, 0xf0c0_0000, 0, VexInterrupt, VexPriority);
+static mut PLIC: Option<PLIC0> = None;
+
 
 static mut PITCH: Option<PitchShift> = None;
 
@@ -180,41 +232,50 @@ fn process(dsp: &mut PitchShift, buf_out: &mut [i16], buf_in: &[i16]) {
 #[export_name = "DefaultHandler"]
 unsafe fn irq_handler() {
 
-    let pending_irq = vexriscv::register::vmip::read();
+    // This should only fail if we have multiple cores claiming things
+    let pending_irq = PLIC0::claim().unwrap();
     let peripherals = pac::Peripherals::steal();
 
-    peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
-    let trace = peripherals.TIMER0.uptime_cycles0.read().bits();
-    LAST_IRQ_PERIOD = trace - LAST_IRQ;
-    LAST_IRQ = trace;
+    match pending_irq.pac_irq {
+        pac::Interrupt::DMA_ROUTER0 => {
+            let offset = peripherals.DMA_ROUTER0.offset_words.read().bits();
+            let pending_subtype = peripherals.DMA_ROUTER0.ev_pending.read().bits();
 
-    if (pending_irq & (1 << pac::Interrupt::DMA_ROUTER0 as usize)) != 0 {
-        let offset = peripherals.DMA_ROUTER0.offset_words.read().bits();
-        let pending_subtype = peripherals.DMA_ROUTER0.ev_pending.read().bits();
+            if let Some(ref mut dsp) = PITCH {
+                if offset as usize == ((BUF_SZ_WORDS/2)+1) {
 
-        if let Some(ref mut dsp) = PITCH {
-            if offset as usize == ((BUF_SZ_WORDS/2)+1) {
-                process(dsp,
-                        &mut BUF_OUT[0..(BUF_SZ_SAMPLES/2)],
-                        &BUF_IN[0..(BUF_SZ_SAMPLES/2)]);
+                    peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
+                    let trace = peripherals.TIMER0.uptime_cycles0.read().bits();
+                    LAST_IRQ_PERIOD = trace - LAST_IRQ;
+                    LAST_IRQ = trace;
+
+                    process(dsp,
+                            &mut BUF_OUT[0..(BUF_SZ_SAMPLES/2)],
+                            &BUF_IN[0..(BUF_SZ_SAMPLES/2)]);
+
+                    peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
+                    let trace_end = peripherals.TIMER0.uptime_cycles0.read().bits();
+                    LAST_IRQ_LEN = trace_end - trace;
+                }
+
+                if offset as usize == (BUF_SZ_WORDS-1) {
+                    process(dsp,
+                            &mut BUF_OUT[(BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES)],
+                            &BUF_IN[(BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES)]);
+                }
             }
 
-            if offset as usize == (BUF_SZ_WORDS-1) {
-                process(dsp,
-                        &mut BUF_OUT[(BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES)],
-                        &BUF_IN[(BUF_SZ_SAMPLES/2)..(BUF_SZ_SAMPLES)]);
-            }
+            peripherals.DMA_ROUTER0.ev_pending.write(|w| w.bits(pending_subtype));
+
+            asm!("fence iorw, iorw");
+            asm!(".word(0x500F)");
+        },
+        _ => {
         }
-
-        peripherals.DMA_ROUTER0.ev_pending.write(|w| w.bits(pending_subtype));
-
-        asm!("fence iorw, iorw");
-        asm!(".word(0x500F)");
     }
 
-    peripherals.TIMER0.uptime_latch.write(|w| w.bits(1));
-    let trace_end = peripherals.TIMER0.uptime_cycles0.read().bits();
-    LAST_IRQ_LEN = trace_end - trace;
+
+    PLIC0::complete(pending_irq);
 }
 
 
@@ -229,7 +290,7 @@ fn main() -> ! {
 
     for i in 0..BUF_SZ_SAMPLES {
         unsafe {
-            BUF_OUT[i] = (i*256) as i16;
+            BUF_OUT[i] = (i*32) as i16;
         }
     }
 
@@ -242,8 +303,12 @@ fn main() -> ! {
         peripherals.DMA_ROUTER0.enable.write(|w| w.bits(1u32));
         peripherals.DMA_ROUTER0.ev_enable.write(|w| w.half().bit(true));
 
-        // Enable interrupts from DMA router (vexriscv specific register)
-        vexriscv::register::vmim::write(1 << (pac::Interrupt::DMA_ROUTER0 as usize));
+        // PLIC configuration
+        let mut plic = PLIC0::new();
+        let dma_irq = VexInterrupt::from(pac::Interrupt::DMA_ROUTER0);
+        plic.set_threshold(VexPriority::from(0));
+        plic.set_priority(dma_irq, VexPriority::from(1));
+        plic.enable_interrupt(dma_irq);
 
         // Enable machine external interrupts (basically everything added on by LiteX).
         riscv::register::mie::set_mext();
@@ -256,7 +321,7 @@ fn main() -> ! {
         unsafe {
             asm!("fence iorw, iorw");
             for i in 0..4 {
-                log::info!("{:x}@{:x}", i, BUF_IN[i]);
+                log::info!("{:x}@{:x},{:x}", i, BUF_IN[i], BUF_OUT[i]);
             }
             log::info!("irq_period: {}", LAST_IRQ_PERIOD);
             log::info!("irq_len: {}", LAST_IRQ_LEN);
---
+++ nax.sh
@@ -0,0 +1,1 @@
+python3 example-ecpix-5.py --ecppack-compress --cpu-type naxriscv --csr-svd build/lambdaconcept_ecpix5/csr.svd --build --lx-ignore-deps --timer-uptime --with-rvc --scala-args 'rvc=true,mmu=false'
