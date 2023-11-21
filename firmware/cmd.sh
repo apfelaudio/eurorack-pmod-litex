c666c3ebf7b39cb360a799c57d65856965bfc70b
Sebastian Holzapfel <me@vk2seb.com>
2023-11-21 23:22:47 +0100

    using -linux-gnu gcc toolchain was causing linkage step failures

--- firmware/cmd.sh
+++ firmware/cmd.sh
@@ -1,1 +1,1 @@
-OBJDUMP=riscv64-linux-gnu-objdump BOARD=colorlight_i5 CC_riscv32imac_unknown_none_elf=riscv64-linux-gnu-gcc AR_riscv32imac_unknown_none_elf=riscv64-linux-gnu-ar RUST_BACKTRACE=1 ./build.sh
+OBJCOPY=riscv64-elf-objcopy BOARD=colorlight_i5 CC_riscv32imac_unknown_none_elf=riscv64-elf-gcc AR_riscv32imac_unknown_none_elf=riscv64-elf-ar RUST_BACKTRACE=1 ./build.sh
